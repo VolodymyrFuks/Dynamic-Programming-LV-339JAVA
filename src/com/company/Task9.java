92f55f6ea739159324cf8784924a0aa7347c70fc
Vova Fuks <vovafuks1996@gmail.com>
2018-08-09 12:55:22 +0300

    added solve for 6 task

--- src/com/company/Task9.java
+++ src/com/company/Task9.java
@@ -5,7 +5,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Task9 implements Task {
+public class Task9 implements com.company.Task {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Please, enter number of days:");
